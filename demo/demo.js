7316e95847cb61e678e7ab3a4116255a0fd37b20
Developer <>
2016-06-02 22:41:45 -0400

    add a demo network with the property setter

--- demo/demo.js
+++ demo/demo.js
@@ -11,11 +11,11 @@ function JSONGraph()
 						   {type:"basic/branch",pos:[1200,200]},
 						   {type:"basic/console",pos:[1400,200]},
 						   {type:"basic/console",pos:[1400,300]},
-						   {type:"basic/number",pos:[200,200]},
-						   {type:"basic/number",pos:[200,300]},
-						   {type:"math/operation",pos:[400,200]},
-						   {type:"basic/watch",pos:[700,200]},
-						   {type:"graph/subgraph",pos:[700,300],subgraph:{
+						   {type:"basic/number",pos:[100,200]},
+						   {type:"basic/number",pos:[100,300]},
+						   {type:"math/operation",pos:[300,200]},
+						   {type:"basic/watch",pos:[500,200]},
+						   {type:"graph/subgraph",pos:[500,300],subgraph:{
 						   nodes:[{type:"graph/input",title:"Input",pos:[200,200]},
 								  {type:"basic/watch",pos:[400,200]},
 								  {type:"graph/output",title:"Output",pos:[600,200]}],
@@ -23,8 +23,11 @@ function JSONGraph()
 								  {origin_id:1,origin_slot:0,target_id:2}],
 						   properties:{anumber:1, aboolean:true}
 						   }},
-						   {type:"basic/watch",pos:[900,300]},
-						   {type:"graph/getProperty",pos:[1200,300],title:"astring"}],
+						   {type:"basic/watch",pos:[700,300]},
+						   {type:"graph/getProperty",pos:[1200,300],title:"astring"},
+						   {type:"widget/button",pos:[1000,300]},
+						   {type:"basic/string",pos:[1000,400],value:"!!!"},
+						   {type:"graph/setProperty",pos:[1200,350],title:"astring"}],
 					links:[{origin_id:0,origin_slot:0,target_id:1},
 						   {origin_id:1,origin_slot:0,target_id:2},
 						   {origin_id:1,origin_slot:1,target_id:3},
@@ -33,7 +36,9 @@ function JSONGraph()
 						   {origin_id:6,origin_slot:0,target_id:7},
 						   {origin_id:6,origin_slot:0,target_id:8},
 						   {origin_id:8,origin_slot:0,target_id:9},
-						   {origin_id:10,origin_slot:0,target_id:3,target_slot:1}],
+						   {origin_id:10,origin_slot:0,target_id:3,target_slot:1},
+						   {origin_id:11,origin_slot:0,target_id:13},
+						   {origin_id:12,origin_slot:0,target_id:13,target_slot:1}],
 					properties:{astring:"the string", aboolean:true}
 	});
 }
