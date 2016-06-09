f3e619a09911706d3a74212828ace483e274b0c1
Rhody Lugo <rhodylugo@gmail.com>
2016-06-09 16:49:47 -0400

    add the jscs formatter

--- utils/eslint_reporter.js
+++ utils/eslint_reporter.js
@@ -3,50 +3,55 @@ module.exports = function (results) {
 	var errors = 0;
 	var warnings = 0;
 	var tooMany = false;
-	var summary = '';
+	var line;
 
 	results.some(function (current) {
 		current.messages.some(function (msg) {
-			summary += current.filePath + ':' + msg.line + ':' + msg.column + ': ';
+			line = current.filePath + ':' + msg.line + ':' + msg.column + ': ';
 
 			if (msg.severity === 1) {
-				summary += 'warning';
+				line += 'warning';
 				warnings++;
 			}
 			else if (msg.severity === 2) {
-				summary += 'error';
+				line += 'error';
 				errors++;
 			}
 
-			summary += ': ' + msg.message + '\n';
+			line += ': ' + msg.message;
+
+			console.log(line);
 
 			if (errors === 20)
 				tooMany = true;
+
 			return tooMany;
 		});
 		return tooMany;
 	});
 
 	if (errors > 0 || warnings > 0) {
+
 		if (tooMany)
-			summary += 'fatal error: too many errors emitted, stopping now.\n';
+			line = 'fatal error: too many errors emitted, stopping now.\n';
 
 		if (warnings > 0) {
-			summary += warnings +' warning';
+			line += warnings +' warning';
 			if (warnings > 1) {
-				summary += 's';
-			}
-		}
-		if (errors > 0 && warnings > 0) {
-			summary += ' and ';
-		}
-		if (errors > 0) {
-			summary += errors +' error';
-			if (errors > 1) {
-				summary += 's';
+				line += 's';
 			}
 		}
 
-		return summary + ' generated.';
+		if (errors > 0 && warnings > 0)
+			line += ' and ';
+
+		if (errors > 0) {
+			line += errors +' error';
+			if (errors > 1) {
+				line += 's';
+			}
+		}
+
+		console.log(line);
 	}
 };
